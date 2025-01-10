ee80f2fb798694bb02df3b873f809b86da81de34
Leizhengwang <Leizhengwang@gmail.com>
2025-01-10 16:37:53 -0600

    update

---
+++ test_07_chat_with_memory_rag_continous.py
@@ -0,0 +1,40 @@
+
+from langchain.chains import ConversationalRetrievalChain
+from langchain_community.vectorstores import Chroma
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.memory import ConversationBufferMemory, ConversationSummaryMemory
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+
+print("Loading documents")
+# Loading documents from local disk
+loader = DirectoryLoader('./', glob="*.txt")
+documents = loader.load()
+
+# Split documents into chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+chunks = text_splitter.split_documents(documents)
+
+print("Creating embeddings")
+# Create embeddings for each chunk
+embeddings = OpenAIEmbeddings(api_key = OPENAI_API_KEY)
+vectorstore = Chroma.from_documents(chunks, embeddings)
+retriever = vectorstore.as_retriever()
+
+print("Creating chains")
+llm = ChatOpenAI(api_key = OPENAI_API_KEY)
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+conversation = ConversationalRetrievalChain.from_llm(
+    llm, retriever=retriever, memory=memory, verbose=True)
+
+while(True):
+    user_input = input("> ")
+    result = conversation.invoke(user_input)
+    print(result["answer"])
